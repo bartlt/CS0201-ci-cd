49e98d4f9893809452e45644a6bf089be45a2b4e
niall20 <niall@thebuildingblocks.com>
2024-02-27 23:27:22 +0000

    adding test method

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "<p>Hello, World!</p>"
 
+@app.route("/test")
+def home():
+    return "<p>Test</p>"
+
 if __name__ == '__main__':
     app.run(debug=True)
