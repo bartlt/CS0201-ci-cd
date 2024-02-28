dcba52bc601c95f4c11c29b3809bf1f312c58d77
Default user <jovyan@localhost>
2024-02-28 13:47:04 +0000

    test flask update and hello

--- test_flask.py
+++ test_flask.py
@@ -21,6 +21,24 @@ class FlaskTestCase(unittest.TestCase):
         # assert the response data
         self.assertEqual(response.data, b'<p>Hello, World!</p>')
 
+class FlaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # creates a test client
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    def test_home(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        response = self.app.get('/')
+
+        # assert the status code of the response
+        self.assertEqual(response.status_code, 200)
+
+        # assert the response data
+        self.assertEqual(response.data, b'<p>Hello, World!</p>')
+
 if __name__ == '__main__':
     unittest.main()
 
